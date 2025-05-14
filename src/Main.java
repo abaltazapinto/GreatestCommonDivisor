990615c0675a3178d71e41051758085b3a195fba
abaltazapinto <andrebaltazarpinto@gmail.com>
2025-05-14 14:51:13 +0100

    Find the greatestCommon Divisor within two numbers learn to use the Math.min

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getGreatestCommonDivisor(25, 15));
+        System.out.println(getGreatestCommonDivisor(12, 30));
+        System.out.println(getGreatestCommonDivisor(9, 18));
+        System.out.println(getGreatestCommonDivisor(81, 153));
+    }
+    public static int getGreatestCommonDivisor(int first, int second) {
+        if (first < 10 || second < 10) {
+            return -1;
+        }
+
+        int gcd = 1;
+
+        // checking the smaller of two numbers
+        int min = Math.min(first, second);
+
+        for (int j = 1; j < min; j++) {
+            if (first % j == 0 && second % j == 0) {
+                gcd = j;
+            }
+        }
+        return gcd;
+    }
+}
